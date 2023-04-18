f38afd10673d5e5185e827c2b090c5c318fe482e
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-04-18 16:25:47 +0500

    table format for index project and bugs, able to assign devs /qa while creating project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,78 +1,69 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update destroy]
 
-   before_action :set_project, only: [:show, :edit, :update, :destroy]
-    
-   def index
+  def index
     authorize! :read, Project
 
     if current_user
-        if current_user.user_type == "manager"
-            @projects = current_user.projects 
-            
-          end
-          if current_user.user_type == "QA"
-            @projects = current_user.assigned_projects 
-
-          end
-          if current_user.user_type == "Developer"
-            @projects = current_user.assigned_projects 
-
-          end
+      @projects = current_user.projects if current_user.user_type == 'manager'
+      @projects = current_user.assigned_projects if current_user.user_type == 'QA'
+      @projects = current_user.assigned_projects if current_user.user_type == 'Developer'
     else
-     redirect_to root_path
-    end   
-        #   binding.pry 
-   end
-    def new
-      @project = Project.new
-      authorize! :create, @project
+      redirect_to root_path
     end
+  end
 
-    def create
-      @project = Project.new(project_params)
-      @project.user = current_user
-      authorize! :create, @project
-      if @project.save
-          flash[:success] = "project was created successfully!"
-          redirect_to project_path(@project)
-        else
-          render 'new', status: 300
-        end
-    end
+  def new
+    @project = Project.new
+    authorize! :create, @project
+  end
 
-    def show
-      authorize! :read, @project
+  def create
+    @project = Project.new(params.require(:project).permit(:name, :description))
+    @project.user = current_user
+    authorize! :create, @project
+    if @project.save
+      @project.update(project_params)
+      flash[:success] = 'Project was created successfully!'
+      redirect_to project_path(@project)
+    else
+      render 'new', status: 300
     end
+  end
 
-    def edit
-      authorize! :edit, @project
-    end
+  def show
+    authorize! :read, @project
+  end
 
-    def update
-      authorize! :edit, @project
-        if @project.update(project_params)
-            flash[:success] = "Project was updated successfully!"
-            redirect_to project_path(@project)
-        else
-            render 'edit'
-    
-        end
-    end
+  def edit
+    authorize! :edit, @project
+  end
 
-    def destroy
-        authorize! :destroy, @project
-        @project.destroy
-        flash[:success] = "Project was deleted successfully!"
-        redirect_to projects_path 
-    end
+  def update
+    authorize! :edit, @project
+    if @project.update(project_params)
+      flash[:success] = 'Project was updated successfully!'
+      redirect_to project_path(@project)
+    else
+      render 'edit'
 
-    private
-    def project_params
-        params.require(:project).permit(:name, :description, user_ids: [])
     end
-   
-    def set_project
-            @project=Project.find(params[:id])
-    end
-        
+  end
+
+  def destroy
+    authorize! :destroy, @project
+    @project.destroy
+    flash[:success] = 'Project was deleted successfully!'
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, user_ids: [])
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,82 +1,79 @@
 class TicketsController < ApplicationController
-
-
- def index
-    if  current_user.assigned_projects.map(&:id).include?(params[:id].to_i) || Project.find(params[:id].to_i).user_id == current_user.id
-    @tickets= Project.find(params[:id]).tickets
-    authorize! :read, Ticket
-    @project=params[:id]
+  def index
+    if current_user.assigned_projects.map(&:id).include?(params[:id].to_i) || Project.find(params[:id].to_i).user_id == current_user.id
+      @tickets = Project.find(params[:id]).tickets
+      authorize! :read, Ticket
+      @project = params[:id]
     else
-        flash[:danger]="You are not allowed to view this page"
-        redirect_to root_path 
+      flash[:danger] = 'You are not allowed to view this page'
+      redirect_to root_path
     end
-    # binding.pry
- end
+  end
 
- def show
-    @ticket= Ticket.find(params[:id])
+  def show
+    @ticket = Ticket.find(params[:id])
     authorize! :read, @ticket
+  end
 
- end
-
- def new
-    @project=params[:id]
-    @pro=Project.find(params[:id].to_i)
+  def new
+    @project = params[:id]
+    @pro = Project.find(params[:id].to_i)
     authorize! :create, Ticket
     # binding.pry
-    if current_user.user_type=="QA"
-    @ticket = Ticket.new
+    if current_user.user_type == 'QA'
+      @ticket = Ticket.new
     else
-        redirect_to @root_path
+      redirect_to @root_path
     end
- end
+  end
 
- def create
-    @pro=Project.find(ticket_params[:project_id].to_i)
+  def create
+    @pro = Project.find(ticket_params[:project_id].to_i)
     @ticket = Ticket.new(ticket_params)
     authorize! :create, @ticket
-    @ticket.user_id=current_user.id
+    @ticket.user_id = current_user.id
     # binding.pry
-        if @ticket.save
-            # binding.pry
-          flash[:success] = "Bug was created successfully!"
-          redirect_to ticket_path(@ticket)
-        else
-            # binding.pry
-          render 'new', status: 300
-        end
+    if @ticket.save
+      # binding.pry
+      flash[:success] = 'Bug was created successfully!'
+      redirect_to ticket_path(@ticket)
+    else
+      # binding.pry
+      render 'new', status: 300
+    end
+  end
 
- end
+  def edit
+    @ticket = Ticket.find(params[:id])
+    authorize! :edit, @ticket
+  end
 
- def edit 
-    @ticket=Ticket.find(params[:id])
-    authorize! :edit , @ticket
- end
-
- def update
-    @ticket=Ticket.find(params[:id])
-    authorize! :edit , @ticket
+  def update
+    @ticket = Ticket.find(params[:id])
+    authorize! :edit, @ticket
 
     if @ticket.update(ticket_params)
-        flash[:success] = "Bug was updated successfully!"
-        redirect_to ticket_path(@ticket)
+      flash[:success] = 'Bug was updated successfully!'
+      redirect_to ticket_path(@ticket)
     else
-        render 'edit'
+      render 'edit'
 
     end
-end
+  end
 
- def destroy
-        @ticket=Ticket.find(params[:id])
-        @project=@ticket.project.id
-        authorize! :destroy, @ticket
-        @ticket.destroy
-        flash[:success] = "Bug was updated successfully!"
-        redirect_to tickets_path(id:@project)
- end
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @project = @ticket.project.id
+    authorize! :destroy, @ticket
+    @ticket.destroy
+    flash[:success] = 'Bug was updated successfully!'
+    redirect_to tickets_path(id: @project)
+  end
 
- private
- def ticket_params
-     params.require(:ticket).permit(:title, :description, :category, :status, :deadline, :project_id, :user_id, :image_url, :developer_id)
- end
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :category, :status, :deadline, :project_id, :user_id,
+                                   :image_url, :developer_id)
+  end
 end
